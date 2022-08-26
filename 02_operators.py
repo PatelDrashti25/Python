f46c28636e4e3329795b37304244fdffa52c33a5
PatelDrashti25 <98554551+PatelDrashti25@users.noreply.github.com>
2022-08-26 22:12:59 +0530

    Create 02_operators.py

---
+++ 02_operators.py
@@ -0,0 +1,30 @@
+a = 5
+b = 2
+
+# Arithmetic Operators
+print("The value of 5+2 is: ", 5+2)
+print("The value of 5-2 is: ", 5-2)
+print("The value of 5*2 is: ", 5*2)
+print("The value of 5/2 is: ", 5/2)
+
+# Assignment Operators
+a = 34
+a -= 12
+a *= 12
+a /= 12
+print(a)
+
+# Comparison Operators
+# b = (14<=7)
+# b = (14>=7)
+# b = (14<7)
+# b = (14>7)
+b = (14!=7)
+print(b)        
+
+# Logical Operators
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is", (bool1 and bool2))
+print("The value of bool1 and bool2 is",(bool1 or bool2))
+print("The value of not bool2 is",(not bool2))
