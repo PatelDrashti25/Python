5191cbcc69fbf0d49ef0838cc022a9af34a95efb
PatelDrashti25 <98554551+PatelDrashti25@users.noreply.github.com>
2022-08-26 10:01:55 +0530

    Create 01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,19 @@
+a = '''Harry'''
+# a = "Harry"
+# a = 'Harry'
+b = 352
+c = 45.25
+d = True
+# d = None
+
+# Printing the variables
+print(a)
+print(b)
+print(c)
+print(d)
+
+# Printing the type of variables
+print(type (a))
+print(type (b))
+print(type (c))
+print(type (d))
