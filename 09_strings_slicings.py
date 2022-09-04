e5e848dd5938564ecebfe63b5cdf16170159d44a
PatelDrashti25 <98554551+PatelDrashti25@users.noreply.github.com>
2022-09-04 16:24:27 +0530

    Create 09_strings_slicings.py

---
+++ 09_strings_slicings.py
@@ -0,0 +1,22 @@
+# greeting = "Good Morning, "
+# name = "Harry"
+# print(type(name))
+
+# #Concatenating two strings
+# c = greeting + name
+# print(c)
+
+# name = "Harry"
+# # print(name[4])
+# # name[3] = "d" -->Does not work
+
+# #print(name[1:4])
+# # print(name[1:4])  # is as same as name[0:4]
+# # print(name[1:]) # as same as name[1:5]
+# c = name[-4:-1] # is same is name[1:4]
+# print(c)
+
+name = "HarryIsGood"
+# d = name[0::2]
+d = name[:0:-1]
+print(d)
