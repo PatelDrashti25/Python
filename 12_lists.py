342810afaf0810b50eba69d3c949dfc4fb0f3d1f
PatelDrashti25 <98554551+PatelDrashti25@users.noreply.github.com>
2022-09-07 22:19:25 +0530

    Create 12_lists.py

---
+++ 12_lists.py
@@ -0,0 +1,21 @@
+# Create a list using []
+a = [1, 2, 34, 5, 87]
+
+# Print the list using print() function
+print(a)
+
+# Access using index using a[0], a[1], a[2]
+print(a[2])
+
+# Change the value of list using
+a[0] = 99
+print(a)
+
+# We can create a list with items of different types
+c = [45, "Harry", False, 6.9]
+print(c)
+
+# List Slicing
+friends = ["Harry", "Tom", "Rohan", "Sam", "Divya",45]
+print(friends[0:4])
+print(friends[-4:])
