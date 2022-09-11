2cb92921c93d56cc042d99f6ff36fbd19eac7f5c
PatelDrashti25 <98554551+PatelDrashti25@users.noreply.github.com>
2022-09-11 16:30:06 +0530

    Create 16_dictionary_syntax.py

---
+++ 16_dictionary_syntax.py
@@ -0,0 +1,12 @@
+myDict = {
+    "Fast": "In a ouick Manner",
+    "Harry": "A Coder",
+    "Marks": [1, 5, 2],
+    "anotherdict": {'harry': 'Player'}
+}
+
+# print(myDict['Fast'])
+# print(myDict['Harry'])
+myDict['Marks'] = [45, 78]
+print(myDict['Marks'])
+print(myDict['anotherdict']['harry'])
